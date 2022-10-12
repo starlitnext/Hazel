80b278b22771633f721810698be1f517aebf9083
silentwind <>
2022-10-12 15:48:21 +0800

    Fix: define HZ_ENABLE_ASSERTS

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -10,7 +10,12 @@
 #	error Hazel only supports Windows
 #endif // HZ_PLATFORM_WINDOWS
 
-#ifdef HZ_ENABLE_ASSERTS
+#define HZ_ENABLE_ASSERTS 0
+#ifdef HZ_DEBUG
+#define HZ_ENABLE_ASSERTS 1
+#endif
+
+#if HZ_ENABLE_ASSERTS
 #	define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #	define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else
