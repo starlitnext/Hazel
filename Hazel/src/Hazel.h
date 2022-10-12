081c8411b8164986b6658e63d15f405044660ff7
silentwind <>
2022-10-12 17:29:54 +0800

    Add: Define Key Codes and Mouse Button Codes

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -5,6 +5,10 @@
 #include "Hazel/Layer.h"
 #include "Hazel/Application.h"
 
+#include "Hazel/Input.h"
+#include "Hazel/KeyCodes.h"
+#include "Hazel/MouseButtonCodes.h"
+
 #include "Hazel/ImGui/ImGuiLayer.h"
 
 #include "Hazel/EntryPoint.h"
