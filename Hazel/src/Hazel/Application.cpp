48c8e006ad6559b2ecf2e2604d239c1357896e92
silentwind <>
2022-10-14 18:47:55 +0800

    Fix: Application not rely on glad

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -7,8 +7,6 @@
 #include "Hazel/Renderer/RenderCommand.h"
 #include "Hazel/Renderer/Renderer.h"
 
-#include <glad/glad.h>
-
 namespace Hazel {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
